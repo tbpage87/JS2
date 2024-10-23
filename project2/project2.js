8bbe5c1d2fef17699dcb2652ae9fef866bb14f6e
tbpage87 <tbpage87@gmail.com>
2024-10-23 17:39:25 -0500

    Update project2.js

--- project2/project2.js
+++ project2/project2.js
@@ -2,3 +2,20 @@ var myName = "Tamla Miller";
 var para1 = document.getElementById("p1");
 
 para1.textContent = myName;
+
+var n1 = 23;
+var n2 = 3;
+var numberSum = n1 + n2;
+document.getElementById("p2").textContent = numberSum;
+
+var numberMult = n1 * n2;
+document.getElementById("p3").textContent = numberMult;
+
+var myNameAddNum = myName + n1;
+document.getElementById("p4").textContent = myNameAddNum;
+
+var myNameMultNum = myName * n2;
+document.getElementById("p5").textContent = myNameMultNum;
+
+var ageCompare = 37 > numberMult;
+document.getElementById("p6").textContent = ageCompare;
