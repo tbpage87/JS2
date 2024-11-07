1cd3720836b7869263f743eb834abf730c6a0b12
tbpage87 <tbpage87@gmail.com>
2024-11-07 17:13:55 -0600

    Create chapter3.js

---
+++ chapter3/chapter3.js
@@ -0,0 +1,21 @@
+var fullName = "Tamla Miller";
+console.log(fullName);
+
+var desiredSalary = 200000;
+console.log(desiredSalary);
+
+var vetStatus = false;
+console.log(vetStatus);
+
+var friends = ["Julie", "Mandy", "Liz"];
+console.log(friends);
+
+var friendsSalaries = [100000, 150000, 50000];
+console.log(friendsSalaries);
+
+var friendObj = {
+  firstName: 'Melody',
+  lastName: 'Page',
+  salary: 250000
+};
+console.log(friendObj);
